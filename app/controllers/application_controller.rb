dbc5b7a955e5fa5eb98c6a15ed0866fae82385d0
rgillespie23 <rgillespie@uchicago.edu>
2023-01-20 21:27:33 +0000

    Rock page up and running

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    #write a ton of code to do whatever user wants and then at end redirect
+
+    # redirect_to("https://www.wikipedia.org")
+
+    # render( :html => "<h1> Hello, world!<h1>".html_safe)
+
+    render({ :template => "game_templates/user_rock.html.erb"})
+  end
 end
